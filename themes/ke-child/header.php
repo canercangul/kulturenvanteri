3863f7cb1e63524c05d91b42df1fb01aaa32d003
A. Erdem Şentürk <aerdemsenturk@gmail.com>
2020-05-25 19:50:27 +0200

    Search, Wiki and Filter on header

--- themes/ke-child/header.php
+++ themes/ke-child/header.php
@@ -1,8 +1,6 @@
 <?php
 /**
- * The header for our theme.
- *
- * Displays all of the <head> section and everything up till <div id="content">
+ * The header
  *
  * @package understrap
  */
@@ -10,8 +8,8 @@
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-$container = get_theme_mod( 'understrap_container_type' );
 ?>
+
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
@@ -30,54 +28,105 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 		<a class="skip-link sr-only sr-only-focusable" href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
 
-		<nav class="navbar navbar-expand-md navbar-dark bg-primary fixed-top">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
 
-		<?php if ( 'container' == $container ) : ?>
-			<div class="container">
-		<?php endif; ?>
-
-					<!-- Your site title as branding in the menu -->
+					<!-- Site title -->
 					<?php if ( ! has_custom_logo() ) { ?>
-
 						<?php if ( is_front_page() && is_home() ) : ?>
-
 							<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a></h1>
-
 						<?php else : ?>
-
-							<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
-
+							<a class="navbar-brand d-none d-md-block" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url"><?php bloginfo( 'name' ); ?></a>
+							<a class="navbar-brand d-block d-md-none" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" itemprop="url">Kültür Envanteri Atlası</a>
 						<?php endif; ?>
+					<?php } else { the_custom_logo(); } ?><!-- end custom logo -->
 
-
-					<?php } else {
-						the_custom_logo();
-					} ?><!-- end custom logo -->
-
-					<?php if ( is_archive() ) : ?>
-					<?php the_archive_title( '<a class="navbar-brand text-primary">', '</a>' );?>
+					<!-- https://gist.github.com/sambody/5787954
+					https://developer.wordpress.org/themes/basics/conditional-tags/#check-for-multiple-conditionals -->
+					
+					<!-- Page Title  -->
+					<?php if ( is_tax() ) : ?>
+					<?php the_archive_title( '<a class="d-none d-md-block navbar-brand text-primary">', '</a>' );?>
 					<?php endif; ?>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-					<span class="navbar-toggler-icon"></span>
-				</button>
 
-				<!-- The WordPress Menu goes here -->
-				<?php wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'container_class' => 'collapse navbar-collapse',
-						'container_id'    => 'navbarNavDropdown',
-						'menu_class'      => 'navbar-nav ml-auto',
-						'fallback_cb'     => '',
-						'menu_id'         => 'main-menu',
-						'depth'           => 2,
-						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-					)
-				); ?>
-			<?php if ( 'container' == $container ) : ?>
-			</div><!-- .container -->
-			<?php endif; ?>
+					<!-- Buttons -->
+					<?php if ( is_tax() ): ?>
+						<div class="ml-auto">
+						<button class="btn btn-sm btn-circle btn-primary" data-toggle="modal" data-target="#button_wiki">Wiki</button>
+						<button class="btn btn-sm btn-circle btn-primary ml-1" data-toggle="modal" data-target="#button_search">Ara</button>
+						</div>
+					<?php endif; ?>
 
+
+					<?php if ( is_author() || is_single() ) : ?>
+						<div class="ml-auto">
+							<button class="btn btn-sm btn-circle btn-primary ml-1" data-toggle="modal" data-target="#button_search">Ara</button>
+							<button class="btn btn-sm btn-circle btn-primary ml-1" type="button" onclick="history.go(-1);">←</button>
+						</div>
+					<?php endif; ?>
+
+
+
+					<?php if ( is_page(array('harita','etrafta','test') ) ) : ?>
+						<div class="ml-auto">
+							<button class="btn btn-sm btn-circle btn-primary" data-toggle="modal" data-target="#button_filter">Ara</button>
+							<button class="btn btn-sm btn-circle btn-primary btn-visiblelist ml-1" data-toggle="modal" data-target="#button_visiblelist">Liste</button>
+						</div>
+					<?php endif; ?>
+
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<!-- Menu -->
+					<?php wp_nav_menu(
+						array(
+							'theme_location'  => 'primary',
+							'container_class' => 'collapse navbar-collapse',
+							'container_id'    => 'navbarNavDropdown',
+							'menu_class'      => 'navbar-nav ml-auto',
+							'fallback_cb'     => '',
+							'menu_id'         => 'main-menu',
+							'depth'           => 2,
+							'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+						)
+					); ?>
+					
 		</nav><!-- .site-navigation -->
 
 	</div><!-- #wrapper-navbar end -->
+
+<!-- #wrapper-navbar end -->
+
+<!-- Modal -->
+<div class="modal" id="button_search" tabindex="-1" role="dialog" aria-labelledby="button_searchTitle" aria-hidden="true">
+	<div class="modal-dialog modal-search" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<form action="/harita/" method="get">
+					<div class="input-group">
+						<input id="capitalizeInput" class="field form-control" type="search" placeholder="kültür varlığı adı, şehir veya bir tür girerek arama yapın." name="_ara">
+						<span class="input-group-append"><input class="submit btn btn-primary" id="searchsubmit" type="submit" value="Ara"></span>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+
+	/**
+	 * Input Capitalize
+	 */
+
+	document.getElementById('capitalizeInput').addEventListener("keyup",   () => {
+	var inputValue = document.getElementById('capitalizeInput')['value']; 
+	if (inputValue[0] === ' ') {
+		inputValue = '';
+		} else if (inputValue) {
+		inputValue = inputValue[0].toLocaleUpperCase() + inputValue.slice(1);
+		}
+	document.getElementById('capitalizeInput')['value'] = inputValue;
+	});
+
+</script>
